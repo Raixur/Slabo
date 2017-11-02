e27abbd44d64423c417e406312b08534dd0a66f1
Raixur <raixur@gmail.com>
2017-11-02 13:00:10 +0200

    Initial commit
    * Add OVR SDK.
    * Add VRTK.
    * Add maze.
    * Add flashlight.
    * Add door prototype.
    * Add chain prototype.
    * Initial scene setup.

---
+++ Assets/Scripts/DoorLock.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class DoorLock : MonoBehaviour
+{
+    private bool isLocked;
+    private FixedJoint lockJoint;
+
+    public Rigidbody Door;
+
+    public bool IsLocked
+    {
+        get { return isLocked; }
+        set
+        {
+            if (isLocked != value)
+            {
+                isLocked = value;
+                if (isLocked)
+                    LockDoor();
+                else
+                    UnlockDoor();
+            }
+        }
+    }
+
+    public void Start()
+    {
+        if (IsLocked)
+        {
+            
+        }
+    }
+
+    public void LockDoor()
+    {
+        lockJoint = gameObject.AddComponent<FixedJoint>();
+        lockJoint.connectedBody = Door;
+    }
+
+    private void UnlockDoor()
+    {
+        Destroy(lockJoint);
+    }
+}
