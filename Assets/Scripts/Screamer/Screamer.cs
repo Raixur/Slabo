abc2e36c77fcb94d50f303fbfffe1a5bcc22b1fb
bchwaves <beachwavestudio@gmail.com>
2017-11-16 15:46:05 +0200

    Add screamer prototype system

---
+++ Assets/Scripts/Screamer/Screamer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Screamer : MonoBehaviour
+{
+    [SerializeField] private bool SpawnEnabled = true;
+    [SerializeField] private bool DespawnEnabled = true;
+    [SerializeField] private GameObject character;
+    
+    [SerializeField] private bool isAnimated = false;
+    [SerializeField] private RuntimeAnimatorController animatorController = null;
+    [SerializeField] private string trigger = "OnSpawn";
+    private Animator animator;
+
+    [SerializeField] private float spawnDuration = 0.1f;
+    [SerializeField] private float lifetimeDuration = 0f;
+    [SerializeField] private float despawnDuration = 0.1f;
+
+    public event EventHandler<SpawnEventArgs> Spawn;
+    public event EventHandler<SpawnEventArgs> Despawn;
+
+    public void Start()
+    {
+        character = character ?? gameObject;
+
+        if (isAnimated)
+        {
+            animator = character.GetComponent<Animator>() ?? character.AddComponent<Animator>();
+            animator.runtimeAnimatorController = animatorController ?? new RuntimeAnimatorController();
+        }
+    }
+
+    public void InitSpawn()
+    {
+        StartCoroutine(LifetimeCoroutine());
+    }
+
+    public IEnumerator LifetimeCoroutine()
+    {
+        OnSpawn();
+        yield return new WaitForSeconds(spawnDuration);
+
+        character.SetActive(true);
+        if (isAnimated)
+            animator.SetTrigger(trigger);
+        yield return new WaitForSeconds(lifetimeDuration);
+
+        OnDespawn();
+        character.SetActive(false);
+    }
+
+    protected virtual void OnSpawn()
+    {
+        if (SpawnEnabled && Spawn != null)
+            Spawn(this, new SpawnEventArgs { Duration = spawnDuration + 0.01f });
+    }
+
+    protected virtual void OnDespawn()
+    {
+        if (DespawnEnabled && Despawn != null)
+            Despawn(this, new SpawnEventArgs {Duration = despawnDuration});
+    }
+}
+
+public class SpawnEventArgs : EventArgs
+{
+    public float Duration { get; set; }
+}
---
+++ Assets/Scripts/Screamer/ScreamerSpawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class ScreamerSpawner : MonoBehaviour
+{
+    private const float Angle = 10f;
+
+    [SerializeField] private List<Screamer> screamers;
+    [SerializeField] private List<LightFlickering> affectedLight;
+    [SerializeField] private Transform pointingObject;
+
+    private Queue<Screamer> screamerQueue;
+
+    private bool isActive;
+
+    public void Start()
+    {
+        pointingObject = pointingObject ?? VRTK_SDK_Bridge.GetHeadsetCamera();
+
+        foreach (var screamer in screamers)
+        {
+            screamer.Spawn += (sender, args) =>
+            {
+                affectedLight.ForEach(l => l.FlickerByTime(args.Duration));
+                isActive = true;
+            };
+            screamer.Despawn += (sender, args) => isActive = false;
+        }
+
+        screamerQueue = new Queue<Screamer>(screamers);
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<VRTK_PlayerObject>() != null && IsPointedOnScreamer() && !isActive)
+        {
+            var screamer = screamerQueue.Dequeue();
+            screamer.InitSpawn();
+        }
+    }
+
+    private bool IsPointedOnScreamer()
+    {
+        var destDir = screamerQueue.Peek().transform.position - pointingObject.transform.position; 
+        return Vector3.Angle(destDir, pointingObject.transform.forward) < Angle;
+    }
+}
