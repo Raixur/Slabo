07e9f386c8f333160a610051785e96ca962e3aa9
Anatoliy Bernatovich <raixur@gmail.com>
2017-11-19 23:38:11 +0200

    Add final room
    * Add final room scene.
    * Add light switch with sound.
    * Add enemy with gun and shooting animation with sound.
    * Add final room cutscene script.

---
+++ Assets/Scripts/FinalRoom.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class FinalRoom : MonoBehaviour
+{
+    
+    [SerializeField] private List<LightSwitch> lights;
+    [SerializeField] private float delay = 2f;
+
+    [SerializeField] private Animator shootingAnimator;
+    [SerializeField] private AudioSource switchAudio;
+    [SerializeField] private AudioSource shotAudio;
+    [SerializeField] private float delayBeforeShoot = 1f;
+
+
+    public void Awake()
+    {
+        lights.ForEach(l => l.enabled = false);
+    }
+
+    public void Start()
+    {
+        StartCoroutine(FinalSceneCoroutine());
+    }
+
+    private IEnumerator FinalSceneCoroutine()
+    {
+        yield return new WaitForSeconds(delay);
+        
+        foreach(var l in lights)
+        {
+            l.SwitchLight();
+            yield return new WaitForSeconds(l.Duration);
+        }
+
+        yield return new WaitForSeconds(delayBeforeShoot);
+        shootingAnimator.SetTrigger("Shoot");
+        yield return new WaitForSeconds(0.2f);
+        shotAudio.Play();
+    }
+}
---
+++ Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+[RequireComponent(typeof(AudioSource))]
+public class LightSwitch : MonoBehaviour
+{
+    private new Light light;
+    private new AudioSource audio;
+
+    [SerializeField] private float audioDelay = 0f;
+
+    public float Duration = 1f;
+    
+	// Use this for initialization
+    [UsedImplicitly]
+    private void Awake()
+    {
+        light = GetComponent<Light>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    public void SwitchLight() { StartCoroutine(SwitchLightCoroutine()); }
+
+    private IEnumerator SwitchLightCoroutine()
+    {
+        audio.Play();
+        yield return new WaitForSeconds(audioDelay);
+        light.enabled = !light.enabled;
+    }
+}
