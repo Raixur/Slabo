9dffe72f9892dcdcf0ad86e496d0b8eff68c53c5
bchwaves <beachwavestudio@gmail.com>
2017-11-20 11:55:22 +0200

    Add sound playlists

---
+++ Assets/Scripts/AudioPlaylist.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using JetBrains.Annotations;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioPlaylist : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] list;
+
+    private System.Random rnd = new System.Random();
+    private AudioSource audioSource;
+
+    [UsedImplicitly]
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        StartCoroutine(PlaySoundCoroutine());
+    }
+
+    private IEnumerator PlaySoundCoroutine()
+    {
+        while (true)
+        {
+            var index = rnd.Next(list.Length);
+            var clip = list[index];
+
+            audioSource.clip = clip;
+            audioSource.Play();
+
+            yield return new WaitForSeconds(clip.length);
+        }
+    }
+}
