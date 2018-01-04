e7dc07aee717978258df47395e522711360ee89b
bchwaves <beachwavestudio@gmail.com>
2018-01-04 18:57:59 +0200

    Temporary solution for buttons

---
+++ Assets/Scripts/Controls/ButtonAttacher.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using JetBrains.Annotations;
+using UnityEngine;
+using VRTK;
+
+public class ButtonAttacher : MonoBehaviour
+{
+    [SerializeField] private DigitInputPanel panel = null;
+    private string doorCode;
+
+    [UsedImplicitly]
+    private void Start()
+    {
+        doorCode = CodeGenerator.Instance.DoorCode.Aggregate("", (s, d) => s + d);
+        panel.Display.InputFinished += TryAttach;
+    }
+
+    private void TryAttach(object sender, DisplayPanelEventArgs args)
+    {
+        if (doorCode == args.Value)
+        {
+            var buttons = GetComponentsInChildren<VRTK_Button>();
+            foreach (var button in buttons)
+            {
+                var joint = button.gameObject.GetComponent<Joint>();
+                joint.connectedBody = gameObject.GetComponent<Rigidbody>();
+            }
+        }
+    }
+}
